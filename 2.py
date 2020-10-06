38745d08e692fc9089c0af6976eeda9315176b13
samael142 <samael.env@gmail.com>
2020-10-06 21:50:32 +0300

    Исправил

--- 2.py
+++ 2.py
@@ -2,4 +2,12 @@ time = int(input('Введи время в секундах'))
 seconds = time % 60
 hours = time // 3600
 minutes = (time // 60) - (hours * 60)
+
+if seconds < 10:
+    seconds = '0' + str(seconds)
+if minutes < 10:
+    minutes = '0' + str(minutes)
+if hours < 10:
+    hours = '0' + str(hours)
+
 print('{}:{}:{}'.format(hours, minutes, seconds))
